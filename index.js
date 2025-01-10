43b404e7d8b25dd5821eecb103776460a2f0d808
FahmiAlmsyah <pahmialamsya@gmail.com>
2025-01-10 20:50:01 +0700

    mengubah info cuaca

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ async function getWeather(latitude = -7.2492, longitude = 112.7508) {
         const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,rain,weather_code,wind_speed_10m&hourly=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset&timezone=auto`;
     const response = await fetch(url);
     const data = await response.json();
+
+    // Change weather info
+    const currentTemp = document.querySelector("#temperature");
+    const currentHumidity = document.querySelector("#humidity");
+    const currentWind = document.querySelector("#wind");
+    const currentTempFeel = document.querySelector("#feelLike");
     } catch (error) {
         console.log(error);
       }
